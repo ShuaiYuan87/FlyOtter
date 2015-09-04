08ee2477fbbe3abae0e6b925333bbfc858cbd2b5
Shuo Shen <shen@fb.com>
2015-09-03 21:39:04 -0700

    amend previous revision

---
+++ app/Protocol.js
@@ -0,0 +1,69 @@
+/* @flow */
+
+'use strict';
+
+class Protocol {
+  // Message Type
+  static CHECK_LATENCY: number;
+  static REQUEST: number;
+  static ACK: number;
+  static ACTION: number;
+
+  // Player Actions
+  static PLAY: number;
+  static PAUSE: number;
+  static SEEK: number;
+  static STOP: number;
+  static RELOAD: number;
+
+  // Operations
+  static CREATE_ROOM: string;
+  static LOAD_VIDEO: string;
+
+  static createMessage(
+    ack_msg_id,
+    rid,
+    time,
+    action,
+    vid
+  ): Object {
+    var message = {
+      clientTime: Date.now() / 1000,
+      clientId: rid,
+      playerTime: time,
+      playerAction: action,
+      videoId: vid,
+      ackMsgID: 0,
+      msgType: 0,
+    };
+    if (ack_msg_id) {
+      message.ackMsgID = ack_msg_id;
+      message.msgType = Protocol.ACK;
+    } else {
+      message.msgType = Protocol.REQUEST;
+    }
+    return message;
+  }
+}
+
+Object.assign(
+  Protocol,
+  {
+    CHECK_LATENCY: 1,
+    REQUEST: 2,
+    ACK: 3,
+    ACTION: 4,
+
+    PLAY: 1,
+    PAUSE: 2,
+    SEEK: 3,
+    STOP: 4,
+    RELOAD: 5,
+
+    CREATE_ROOM: 'create',
+    LOAD_VIDEO: 'reload',
+  }
+);
+
+
+module.exports = Protocol;
---
+++ app/RemotePlaybackControl.js
@@ -0,0 +1,96 @@
+/* @flow */
+
+'use strict';
+
+var io = require('socket.io-client');
+var Protocol = require('./Protocol.js');
+
+var UNIVERSE = 1024;
+
+class RemotePlaybackControl {
+  onPlay: () => void;
+  onLoadVideo: (videoURL: string) => void;
+  onSeek: (sec: number) => void;
+
+  socket: any;
+  roomID: number;
+  clientID: number;
+
+  constructor(host: string, port: number, roomID: number) {
+    this.socket = io.connect(`http://${host}:${port.toString()}`);
+    this.socket.emit(Protocol.CREATE_ROOM, `room${roomID}`);
+    this.roomID = roomID;
+    this.clientID = Math.floor(Math.random() * UNIVERSE);
+    console.log('construct');
+
+    this.socket.on(
+      Protocol.LOAD_VIDEO,
+      (response) => {
+        var data = JSON.parse(response.message);
+        if (parseInt(data.clientId) === this.clientID) {
+          return;
+        }
+        this.onLoadVideo && this.onLoadVideo(data.videoId);
+      }
+    );
+
+    this.socket.on(
+      'notification',
+      (response) => {
+        var data = JSON.parse(response.message);
+        if (parseInt(data.clientId) === this.clientID) {
+          return;
+        }
+
+        switch(data.msgType) {
+        case Protocol.ACTION:
+          this.onPlay && this.onPlay();
+          break;
+        }
+        return;
+      }
+    );
+  }
+
+  loadVideo(videoUrl: string): void {
+    var message = Protocol.createMessage(
+      false,
+      this.clientID,
+      0,
+      Protocol.RELOAD,
+      videoUrl
+    );
+    this.socket.emit(Protocol.LOAD_VIDEO, JSON.stringify(message));
+  }
+
+  // false, rid, 0, PlayerAction.PLAY);
+  play(): void {
+    var message = Protocol.createMessage(
+      false,
+      this.clientID,
+      0,
+      Protocol.PLAY
+    );
+    this.socket.emit('postData', JSON.stringify(message));
+  }
+
+  pause(): void {
+    var message = Protocol.createMessage(
+      false,
+      this.clientID,
+      0,
+      Protocol.PAUSE
+    );
+    this.socket.emit('postData', JSON.stringify(message));
+  }
+
+  toggle(): void {
+    this.play();
+  }
+
+  disconnect(): void {
+
+  }
+}
+
+module.exports = RemotePlaybackControl;
