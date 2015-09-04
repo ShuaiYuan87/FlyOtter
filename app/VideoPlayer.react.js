6a8cd7743d1f8558a5791e3679d547251ea622a1
Shuo Shen <shen@fb.com>
2015-09-03 21:38:27 -0700

    synced load video and toggle

--- app/VideoPlayer.react.js
+++ app/VideoPlayer.react.js
@@ -7,17 +7,40 @@
 
 var PlaybackControl = require('./PlaybackControl');
 var React = require('react');
+var RemotePlaybackControl = require('./RemotePlaybackControl');
 var StyleSheet = require('react-style')
 
 var VIDEO_PLAYER_ID = 'keekwoon-player';
+//var serverIP = '73.231.32.235';
+var HOST = 'localhost';
+var PORT = 8989;
+var ROOM_ID = 1234;
 
 var VideoPlayer = React.createClass({
   getInitialState: function(): Object {
+    var remote = new RemotePlaybackControl(
+      HOST,
+      PORT,
+      ROOM_ID
+    );
+
+    remote.onLoadVideo = (videoURL) => {
+      console.log('onload video')
+      this.setState({
+        videoURL: videoURL,
+      });
+      PlaybackControl.getControl().loadVideo(VIDEO_PLAYER_ID, videoURL);
+    };
+    remote.onPlay = () => {
+      PlaybackControl.getControl().toggle();
+    };
+
     return {
       videoURL: '',
       playerSec: 0,
       currentTime: 0,
       isControlPanelVisible: false,
+      remotePlaybackControl: remote,
     };
   },
 
@@ -59,20 +82,24 @@ var VideoPlayer = React.createClass({
   },
 
   _togglePlayerState: function(): void {
+    this.state.remotePlaybackControl.toggle();
     PlaybackControl.getControl().toggle();
   },
 
   _loadVideo: function(evt: Object): void {
     var playbackControl = PlaybackControl.getControl();
+    this.state.remotePlaybackControl.loadVideo(this.state.videoURL);
     playbackControl.loadVideo(VIDEO_PLAYER_ID, this.state.videoURL);
     playbackControl.playerTick = (time) => this.setState({currentTime: time});
   },
 
   _playVideo: function(evt: Object): void {
+    this.state.remotePlaybackControl.play();
     PlaybackControl.getControl().play();
   },
 
   _pauseVideo: function(evt: Object): void {
+    this.state.remotePlaybackControl.pause();
     PlaybackControl.getControl().pause();
   },
 
